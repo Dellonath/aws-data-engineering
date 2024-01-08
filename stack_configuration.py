20ef495186b34ebbbd86efc29a24e01082ebe30c
Douglas <dellonath@gmail.com>
2024-01-08 17:24:06 -0300

    feature/adding tests and deploy steps on actions

--- stack_configuration.py
+++ stack_configuration.py
@@ -4,7 +4,7 @@ class CloudFormationStackConfiguration():
     
     def __init__(self):
      
-        self.aws_account_id = '975050223655' # os.environ["ACCOUNT_ID"]
+        self.aws_account_id = os.environ["ACCOUNT_ID"]
         self.aws_region = "us-east-1"
-        self.environment = 'dev' # os.environ["ENVIRONMENT"]
+        self.environment = os.environ["ENVIRONMENT"]
         
