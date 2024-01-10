0535efbb1ae8fc2bff2a1c8f7d7355e9ef582363
Douglas <dellonath@gmail.com>
2024-01-09 22:43:46 -0300

    test/aws cdk on github actions

--- stack_configuration.py
+++ stack_configuration.py
@@ -4,7 +4,7 @@ class CloudFormationStackConfiguration():
     
     def __init__(self):
      
-        self.aws_account_id = os.environ["AWS_ACCOUNT_ID"]
+        self.aws_account_id = os.environ["ACCOUNT_ID"]
         self.aws_region = "us-east-1"
         self.environment = os.environ["ENVIRONMENT"]
         
